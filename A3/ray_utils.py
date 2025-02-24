31830dcc6efad1e2866db4e2cd665985ab9f9ed4
shahram95 <shahramnajam.neduet@gmail.com>
2025-02-24 07:28:10 -0500

    Added solution for 1.3

--- A3/ray_utils.py
+++ A3/ray_utils.py
@@ -89,10 +89,10 @@ def get_pixels_from_image(image_size, camera):
     W, H = image_size[0], image_size[1]
 
     # TODO (Q1.3): Generate pixel coordinates from [0, W] in x and [0, H] in y
-    pass
+    x, y = torch.arange(0, W), torch.arange(0, H)
 
     # TODO (Q1.3): Convert to the range [-1, 1] in both x and y
-    pass
+    x, y = 2 * (x / W) - 1, 2 * (y / H) - 1
 
     # Create grid of coordinates
     xy_grid = torch.stack(
@@ -119,7 +119,7 @@ def get_rays_from_pixels(xy_grid, image_size, camera):
     W, H = image_size[0], image_size[1]
 
     # TODO (Q1.3): Map pixels to points on the image plane at Z=1
-    pass
+    ndc_points = xy_grid.cuda()
 
     ndc_points = torch.cat(
         [
@@ -130,13 +130,14 @@ def get_rays_from_pixels(xy_grid, image_size, camera):
     )
 
     # TODO (Q1.3): Use camera.unproject to get world space points from NDC space points
-    pass
+    world_pts= camera.unproject_points(ndc_points, world_coordinates=True, from_ndc=True)
 
     # TODO (Q1.3): Get ray origins from camera center
-    pass
+    camera_center = camera.get_camera_center()
+    rays_o = camera_center.expand(world_pts.shape[0], -1)
 
     # TODO (Q1.3): Get ray directions as image_plane_points - rays_o
-    pass
+    rays_d = F.normalize(world_pts - rays_o)
 
     # Create and return RayBundle
     return RayBundle(
--- A3/volume_rendering_main.py
+++ A3/volume_rendering_main.py
@@ -98,12 +98,16 @@ def render_images(
 
         # TODO (Q1.3): Visualize xy grid using vis_grid
         if cam_idx == 0 and file_prefix == '':
-            pass
+            img = vis_grid(xy_grid, image_size)
+            plt.imsave('images/1_3_xygrid.png', img)
 
         # TODO (Q1.3): Visualize rays using vis_rays
         if cam_idx == 0 and file_prefix == '':
-            pass
-        
+            print('Here')
+            img = vis_rays(ray_bundle, image_size)
+            print('Here')
+            plt.imsave('images/1_3_rays.png', img)
+            print('Here')
         # TODO (Q1.4): Implement point sampling along rays in sampler.py
         pass
 
