438783cc7c287a9be0b444ca7d2e81b7211a835e
shahram95 <shahramnajam.neduet@gmail.com>
2025-03-23 00:16:21 -0400

    Removed *.sh files from code submission

--- A4/snsyed_proj4_code/Q2/q2_1.sh
+++
@@ -1,8 +0,0 @@
-python Q21_image_optimization.py --prompt="a hamburger" --sds_guidance=0
-python Q21_image_optimization.py --prompt="a hamburger" --sds_guidance=1 --postfix="_guidance"
-python Q21_image_optimization.py --prompt="a standing corgi dog" --sds_guidance=0
-python Q21_image_optimization.py --prompt="a standing corgi dog" --sds_guidance=1 --postfix="_guidance"
-python Q21_image_optimization.py --prompt="piano playing shiba inu dog" --sds_guidance=0
-python Q21_image_optimization.py --prompt="piano playing shiba inu dog" --sds_guidance=1 --postfix="_guidance"
-python Q21_image_optimization.py --prompt="a water type firefighting pokemon" --sds_guidance=0
-python Q21_image_optimization.py --prompt="a water type firefighting pokemon" --sds_guidance=1 --postfix="_guidance"
--- A4/snsyed_proj4_code/Q2/q2_2.sh
+++
@@ -1,2 +0,0 @@
-python Q22_mesh_optimization.py --prompt="cat playing computer games"
-python Q22_mesh_optimization.py --prompt="a hamburger"
