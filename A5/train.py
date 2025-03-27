ab2923f4ae14058e7a8f8471f94a1ff31be62fb6
shahram95 <shahramnajam.neduet@gmail.com>
2025-03-27 06:53:44 -0400

    Completed train.py code

--- A5/train.py
+++ A5/train.py
@@ -20,7 +20,7 @@ def train(train_dataloader, model, opt, epoch, args, writer):
         labels = labels.to(args.device).to(torch.long)
 
         # ------ TO DO: Forward Pass ------
-        predictions = 
+        predictions = model(point_clouds)
 
         if (args.task == "seg"):
             labels = labels.reshape([-1])
@@ -55,7 +55,8 @@ def test(test_dataloader, model, epoch, args, writer):
 
             # ------ TO DO: Make Predictions ------
             with torch.no_grad():
-                pred_labels = 
+                pred_labels = model(point_clouds)
+                pred_labels = torch.argmax(pred_labels, dim=1)
             correct_obj += pred_labels.eq(labels.data).cpu().sum().item()
             num_obj += labels.size()[0]
 
@@ -74,7 +75,8 @@ def test(test_dataloader, model, epoch, args, writer):
 
             # ------ TO DO: Make Predictions ------
             with torch.no_grad():     
-                pred_labels = 
+                pred_labels = model(point_clouds)
+                pred_labels = torch.argmax(pred_labels, dim=2)
 
             correct_point += pred_labels.eq(labels.data).cpu().sum().item()
             num_point += labels.view([-1,1]).size()[0]
@@ -99,9 +101,9 @@ def main(args):
 
     # ------ TO DO: Initialize Model ------
     if args.task == "cls":
-        model = 
+        model = cls_model().to(args.device)
     else:
-        model = 
+        model = seg_model(num_seg_classes=args.num_seg_class).to(args.device)
     
     # Load Checkpoint 
     if args.load_checkpoint:
